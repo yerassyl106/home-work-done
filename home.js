41377e91400a6f3b53ae6ed2326390f3f6db5d6a
yerassyl106 <yerassyl106@gmail.com>
2024-10-31 19:00:17 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+// 1-task
+let  slicesPerPizza = 8
+let numberOfPizza = 5
+
+var totalSlices = slicesPerPizza * numberOfPizza
+
+console.log("1-task:",totalSlices);
+
+
+// 2-task
+let = totalChocolates = 24
+let = numberOfFriends = 6
+
+var chocolatesPerFriend = totalChocolates / numberOfFriends
+
+console.log("2-task:",chocolatesPerFriend);
+
+// 3-task
+let plantHeight = 10; 
+
+
+plantHeight += 2; 
+
+
+console.log( "3-task:",plantHeight );
